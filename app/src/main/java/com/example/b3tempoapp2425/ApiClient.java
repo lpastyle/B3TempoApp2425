fc41ec0d2e7c6056515672ac32ff1c2f7040e73f
lpastyle <lpastorelli@lpastyle.com>
2025-06-26 11:48:58 +0200

    Added retrofit init

--- app/src/main/java/com/example/b3tempoapp2425/ApiClient.java
+++ app/src/main/java/com/example/b3tempoapp2425/ApiClient.java
@@ -1,4 +1,56 @@
 package com.example.b3tempoapp2425;
 
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    private static final String LOG_TAG=ApiClient.class.getSimpleName();
+    private static final String EDFAPI_BASE_URL = "https://api-commerce.edf.fr";
+
+    private static Retrofit retrofit = null;
+
+    static Retrofit get() {
+        // Create a HTTP Header interceptor
+        Interceptor headerInterceptor = new Interceptor() {
+            @NonNull
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                Request original = chain.request();
+                Request request = original.newBuilder()
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "application/json, text/plain, */*")
+                        .header("Origin","https://particulier.edf.fr")
+                        .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15")
+                        .header("Referer","https://particulier.edf.fr/")
+                        .header("Situation-Usage","saison")
+                        .header("X-Request-Id", String.valueOf(System.currentTimeMillis()))
+                        .header("Application-Origine-Controlee", "site_RC")
+                        .method(original.method(), original.body())
+                        .build();
+                return chain.proceed(request);
+            }
+        };
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .build();
+
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(EDFAPI_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit;
+    }
 }
