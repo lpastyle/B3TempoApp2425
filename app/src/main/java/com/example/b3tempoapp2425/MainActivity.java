46f4a7f73ac7607d10f4c0310cb0f1ff1fc1e0b7
lpastyle <lpastorelli@lpastyle.com>
2025-06-26 12:21:59 +0200

    Added INTERNET permission

--- app/src/main/java/com/example/b3tempoapp2425/MainActivity.java
+++ app/src/main/java/com/example/b3tempoapp2425/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+        updateNbTempoDaysLeft();
+
     }
     /*
      *  ----------------------- Helper Methods -------------------------
